aa54760252b968675b688d1339a921a912f96ed5
Kasualkid12 <kasu@kasu-it.dev>
2023-06-07 16:23:58 -0500

    fixed build in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/index.js',
 
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
 
